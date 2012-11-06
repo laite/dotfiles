95f102973c9e0579c8857c44847b7d39898184fb
Markus Klinga <laite@gmx.com>
2012-11-06 20:30:34 +0200

    just playing

--- exercises/C++ Primer/Chapter 11/testail/main.cpp
+++ exercises/C++ Primer/Chapter 11/testail/main.cpp
@@ -4,9 +4,6 @@
 
 int main()
 {
-	int a;
-	cout << a;
-
 	list<string> stringList { "this", "that", "those", "them", "dem" };
 	vector<const char*> charVector { "this", "that", "nothing", "more" };
 
@@ -27,6 +24,20 @@ int main()
 	string result = accumulate(s.begin(), s.end(), basic);
 
 	cout << result << endl;
+
+	/* checking replace_copy() and back_inserter (nice!) */
+
+	vector<string> sVec;
+
+	// replace_copy takes five parameters: begin and end of what to copy, destination iter (here back_inserter)
+	// and what should be replaced to what
+	// note that without back_inserter we must have enough items allocated in sVec!
+	replace_copy(stringList.begin(), stringList.end(), back_inserter(sVec), "that", "what");
+
+	for (vector<string>::iterator i = sVec.begin(); i != sVec.end(); )
+		cout << *(i++) << " ";
+
+	cout << endl;
 
 	return 0;
 }
