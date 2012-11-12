6f7faec5497bae0168dd58b141e7e59b6d27ac90
Markus Klinga <laite@gmx.com>
2012-11-12 10:27:11 +0200

    small change: underscore, grave & acute seems proper (for now)

--- xmodmaps.sh
+++ xmodmaps.sh
@@ -3,4 +3,4 @@
 xmodmap -e "keycode 16 = 7 underscore 7 slash braceleft NoSymbol braceleft"
 xmodmap -e "keycode 61 = slash minus underscore endash dead_belowcomma endash"
 xmodmap -e 'keycode 35 = quotedbl asciicircum quotedbl asciicircum asciitilde dead_macron dead_tilde dead_macron'
-xmodmap -e 'keycode 21 = percent underscore percent underscore grave equal grave equal'
+xmodmap -e 'keycode 21 = underscore acute underscore acute grave equal grave equal'
