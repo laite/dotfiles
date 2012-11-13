3d2a8072b0d835ae165fb22edac94e31af5baf1e
Markus Klinga <laite@gmx.com>
2012-11-13 11:31:56 +0200

    more keymappings for the leet

--- xmodmaps.sh
+++ xmodmaps.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+# key definitions for 1337 keyboard usage
+# "original" (finnish) meanings of keys abbreviated in comments
 
-xmodmap -e "keycode 16 = 7 underscore 7 slash braceleft NoSymbol braceleft"
-xmodmap -e "keycode 61 = slash minus underscore endash dead_belowcomma endash"
-xmodmap -e 'keycode 35 = quotedbl asciicircum quotedbl asciicircum asciitilde dead_macron dead_tilde dead_macron'
-xmodmap -e 'keycode 21 = underscore acute underscore acute grave equal grave equal'
+# 16 = number 7
+xmodmap -e 'keycode 16 = 7 underscore 7 slash braceleft NoSymbol braceleft'
+# key 21 = left to backspace
+xmodmap -e 'keycode 21 = asciitilde acute asciitilde acute grave equal grave equal'
+
+# key 34 = swedish 'o'
+xmodmap -e 'keycode 34 = bracketleft braceleft bracketleft braceleft parenleft dead_abovering parenright dead_abovering'
+# key 35 = dead_circum/dead_tilde
+xmodmap -e 'keycode 35 = bracketright braceright bracketright braceright parenright dead_abovering parenright dead_abovering'
+
+# key 49 = section/half
+xmodmap -e 'keycode 49 = asciicircum dollar asciicircum dollar 0 NoSymbol 0 NoSymbol'
+# key 51 = apostrophe/asterisk
+xmodmap -e 'keycode 51 = apostrophe asterisk apostrophe asterisk quotedbl dead_caron quotedbl dead_breve'
+
+# key 61 = (line)
+xmodmap -e 'keycode 61 = slash minus underscore slash underscore dead_belowcomma underscore dead_belowcomma'
