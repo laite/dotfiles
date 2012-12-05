16a739c8fe63ea40f8d0409adcb9d7d1e4a219d4
Markus Klinga <laite@gmx.com>
2012-12-05 10:26:50 +0200

    added helper script

---
+++ shortcuts.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+backupped="Made backups for the following file(s): "
+
+if [ -f ~/.vimrc ]; then
+	mv ~/.vimrc ~/.vimrc_backup
+	backupped="$backupped .vimrc"
+fi
+
+if [ -f ~/.gvimrc ]; then
+	mv ~/.gvimrc ~/.gvimrc_backup
+	backupped="$backupped .gvimrc"
+fi
+
+if [ -f ~/.numbermaps.vim ]; then
+	mv ~/.numbermaps.vim ~/.numbermaps.vim_backup
+	backupped="$backupped .numbermaps.vim"
+fi
+
+if [ -f ~/.bashrc ]; then
+	mv ~/.bashrc ~/.bashrc_backup
+	backupped="$backupped .bashrc"
+fi
+
+if [ -f ~/.bash_aliases ]; then
+	mv ~/.bash_aliases ~/.bash_aliases_backup
+	backupped="$backupped .bash_aliases"
+fi
+
+if [ -f ~/.bash_functions ]; then
+	mv ~/.bash_functions ~/.bash_functions_backup
+	backupped="$backupped .bash_functions"
+fi
+
+if [ -f ~/.Xresources ]; then
+	mv ~/.Xresources ~/.Xresources_backup
+	backupped="$backupped .Xresources"
+fi
+
+ln -s /home/laite/workspace/dotfiles/vimrc ~/.vimrc
+ln -s /home/laite/workspace/dotfiles/gvimrc ~/.gvimrc
+ln -s /home/laite/workspace/dotfiles/numbermaps.vim ~/.numbermaps.vim
+ln -s /home/laite/workspace/dotfiles/bashrc ~/.bashrc
+ln -s /home/laite/workspace/dotfiles/bash_aliases ~/.bash_aliases
+ln -s /home/laite/workspace/dotfiles/bash_functions ~/.bash_functions
+ln -s /home/laite/workspace/dotfiles/Xresources ~/.Xresources
+
+echo "All is well"
+echo $backupped
