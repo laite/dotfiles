114c9f1b7ceb95eb0503d5ac9e49d58ce73faec8
Markus Klinga <laite@gmx.com>
2012-12-12 14:22:45 +0200

    change vim plugin list into a bash script

---
+++ vim_plugins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# this script installs my favourite vim scripts from github
+
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+
+# Pathogen
+git clone https://github.com/tpope/vim-pathogen.git
+cp ./vim-pathogen.git/autoload/pathogen.vim ~/.vim/autoload
+
+# Tagbar
+git clone git://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar 
+
+# EasyMotion
+git clone https://github.com/Lokaltog/vim-easymotion.git ~/.vim/bundle/vim-easymotion.git 
+
+# YankRing
+git clone https://github.com/vim-scripts/YankRing.vim.git ~/.vim/bundle/YankRing.vim.git 
+
+# SuperTab
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab.git 
+
+# SnipMate
+git clone https://github.com/msanders/snipmate.vim.git ~/.vim/bundle/snipmate.vim.git 
