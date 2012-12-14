6e5d1f88d4fa077f58ce3ffab643f175b30b5885
Markus Klinga <laite@laite-mint.(none)>
2012-12-14 13:57:32 +0200

    fix pathogen directory, also unzip colors

--- vim_plugins.sh
+++ vim_plugins.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # this script installs my favourite vim scripts from github
 
+mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/bundle
 
 # Pathogen
 git clone https://github.com/tpope/vim-pathogen.git
-cp ./vim-pathogen.git/autoload/pathogen.vim ~/.vim/autoload
+cp ./vim-pathogen/autoload/pathogen.vim ~/.vim/autoload
+rm -rf vim-pathogen
 
 # Tagbar
 git clone git://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar 
@@ -19,3 +21,6 @@ git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
 
 # SnipMate
 git clone https://github.com/msanders/snipmate.vim.git ~/.vim/bundle/snipmate.vim
+
+# Also install all the pretty colors
+unzip vimColorSamplePack.zip -d ~/.vim
