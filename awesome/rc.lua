c0a5cb6dd155484be3008d580c85030f29acd5b6
Markus Klinga <laite@gmx.com>
2012-12-18 12:48:11 +0200

    bring back mod+tab, add a space after layout indicator texts

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -257,7 +257,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
     --awful.key({ modkey,           }, "m", function () awful.screen.focus_relative( 1) end),
     awful.key({ modkey, "Shift"   }, "u", awful.client.urgent.jumpto),
-    awful.key({ modkey,           }, "m",
+    awful.key({ modkey,           }, "Tab",
         function ()
             awful.client.focus.history.previous()
             if client.focus then
