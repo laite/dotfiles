512f32e2e08c7fd0d6e187a65b9877bbc973704e
Markus Klinga <laite@gmx.com>
2013-05-06 11:28:26 +0300

    äänestyslomake, vrsio1

---
+++ exercises/maaseutu/lomake.php
@@ -0,0 +1,65 @@
+
+     <h4>KATEGORIA 1 </h4>
+<table>
+    <tr>
+    <td> Nimi: </td>
+    <td> <input name="nimi" size="30"/></td>
+    </tr>
+	<tr>
+	<td> Paikkakunta: </td>
+    <td> <input name="paikkakunta"/></td>
+	</tr> 
+    <tr> 
+	<td> Perustelut: </td> 
+	<td> <textarea rows = "4" cols="45" maxlength="1000"> </textarea> </td> 
+    </tr> 
+</table>   
+
+     <h4>KATEGORIA 2 </h4>
+<table>
+    <tr>
+    <td> Nimi: </td>
+    <td> <input name="nimi" size="30"/></td>
+    </tr>
+	<tr>
+	<td> Paikkakunta: </td>
+    <td> <input name="paikkakunta" size="10"/></td>
+	</tr> 
+    <tr> 
+	<td> Perustelut: </td> 
+	<td> <textarea rows = "4" cols="45"> </textarea> </td> 
+    </tr> 
+</table>  
+    
+	<h4>KATEGORIA 3 </h4>
+<table>
+    <tr>
+    <td> Nimi: </td>
+    <td> <input name="nimi" size= "60" /></td>
+    </tr>
+	<tr>
+	<td> Paikkakunta: </td>
+    <td> <input name="paikkakunta" size= "60" /></td>
+	</tr> 
+    <tr> 
+	<td> Perustelut: </td> 
+	<td> <textarea rows = "4" cols="45"> </textarea> </td> 
+    </tr> 
+</table>
+ 
+ <h4>KATEGORIA 4 </h4>
+<table>
+    <tr>
+    <td> Nimi: </td>
+    <td> <input name="nimi" size= "10" /></td>
+    </tr>
+	<tr>
+	<td> Paikkakunta: </td>
+    <td> <input name="paikkakunta" size= "10" /></td>
+	</tr> 
+    <tr> 
+	<td> Perustelut: </td> 
+	<td> <textarea rows = "4" cols="45"> </textarea> </td> 
+    </tr> 
+</table>
+
