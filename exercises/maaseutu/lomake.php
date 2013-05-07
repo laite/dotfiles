55e429eddc8832e7cbda161b0605dd41a8fff67d
Markus Klinga <laite@gmx.com>
2013-05-07 13:50:17 +0300

    lomake-ehdotus, versio2

--- exercises/maaseutu/lomake.php
+++ exercises/maaseutu/lomake.php
@@ -1,65 +1,91 @@
+<p>
+Alla ovat tämän vuoden ehdotuskategoriat, joihin voit äänestää omia suosikkejasi. Voit vastata vain niihin kategorioihin, joihin haluat ja jättää muut tyhjäksi. Muista painaa sivun alalaidasta löytyvää "lähetä ehdotukset" -nappia kirjoitettuasi, jotta ehdotukset rekisteröityvät.
+</p>
+<p style="color: red;">
+Mari: Tämä on vain ehdotus, saat tietysti keksiä tähän paremmankin "pohjustustekstin". :)
+</p>
 
-     <h4>KATEGORIA 1 </h4>
+<form method="post" action="" />
+<h2><strong>Innostava maaseudun kehittäjä</strong></h2>
+<p>
+Henkilöllä on ennakkoluuloton asenne maaseudun kehittämiseen ja hän tuo aktiivisesti esille uusia näkökulmia maaseudun elinvoimaisuuden hyväksi. Henkilö on edesauttanut maaseudun verkostoitumista ja parantanut paikallista yhteisöllisyyttä.
+</p>
 <table>
     <tr>
     <td> Nimi: </td>
-    <td> <input name="nimi" size="30"/></td>
-    </tr>
-	<tr>
-	<td> Paikkakunta: </td>
-    <td> <input name="paikkakunta"/></td>
-	</tr> 
-    <tr> 
-	<td> Perustelut: </td> 
-	<td> <textarea rows = "4" cols="45" maxlength="1000"> </textarea> </td> 
+    <td> <input name="nimi1" size="30" maxlength="100"/></td>
+    </tr><tr>
+    <td> Paikkakunta: </td>
+    <td> <input name="paikkakunta3" size="30" maxlength="50"/></td>
+    </tr><tr> 
+    <td> Perustelut: </td> 
+    <td> <textarea name="perustelut1" rows="4" cols="45" maxlength="1000"> </textarea> </td> 
     </tr> 
 </table>   
-
-     <h4>KATEGORIA 2 </h4>
+<h2><strong>Oivallinen maaseututeko</strong></h2>
+<p>
+Vuoden maaseututeko 2013 voi olla hanke, tapahtuma, yhteisö, tempaus tai projekti. Teko on tuonut esille keskisuomalaista maaseutua uudella tavalla, sekä lisännyt sen yhteisöllisyyttä, näkyvyyttä ja elinvoimaa.
+</p>
 <table>
     <tr>
     <td> Nimi: </td>
-    <td> <input name="nimi" size="30"/></td>
-    </tr>
-	<tr>
-	<td> Paikkakunta: </td>
-    <td> <input name="paikkakunta" size="10"/></td>
-	</tr> 
-    <tr> 
-	<td> Perustelut: </td> 
-	<td> <textarea rows = "4" cols="45"> </textarea> </td> 
+    <td> <input name="nimi2" size="30" maxlength="100"/></td>
+    </tr><tr>
+    <td> Paikkakunta: </td>
+    <td> <input name="paikkakunta2" size="30" maxlength="50"/></td>
+    </tr><tr> 
+    <td> Perustelut: </td> 
+    <td> <textarea name="perustelut2" rows="4" cols="45" maxlength="1000"> </textarea> </td> 
     </tr> 
-</table>  
-    
-	<h4>KATEGORIA 3 </h4>
+</table>   
+<h2><strong>Onnistunut ympäristöpanostus</strong></h2>
+<p>
+Vuoden 2013 ympäristöpanostus voi olla yksityisen henkilön, porukan, yhteisön tai yrityksen panostus kestävän kehityksen ja ympäristön hyväksi. Ympäristö on voitu huomioida esimerkiksi ideoimalla uutta perinteisessä maataloudessa tai uudella innovatiivisella tavalla muussa yritystoiminnassa.
+</p>
 <table>
     <tr>
     <td> Nimi: </td>
-    <td> <input name="nimi" size= "60" /></td>
-    </tr>
-	<tr>
-	<td> Paikkakunta: </td>
-    <td> <input name="paikkakunta" size= "60" /></td>
-	</tr> 
-    <tr> 
-	<td> Perustelut: </td> 
-	<td> <textarea rows = "4" cols="45"> </textarea> </td> 
+    <td> <input name="nimi3" size="30" maxlength="100"/></td>
+    </tr><tr>
+    <td> Paikkakunta: </td>
+    <td> <input name="paikkakunta3" size="30" maxlength="50"/></td>
+    </tr><tr> 
+    <td> Perustelut: </td> 
+    <td> <textarea name="perustelut3" rows="4" cols="45" maxlength="1000"> </textarea> </td> 
+    </tr> 
+</table>   
+<h2><strong>Vuoden 2013 maatalousyritys</strong></h2>
+<p>
+Tila on edistänyt maatalouden säilymistä ja maaseudun elinvoimaisuutta sekä kehittänyt toimintaansa suunnitelmallisesti. Lisäksi se on löytänyt uusia keinoja elinkeinon harjoittamiseen ja hyödyntänyt verkostoja järkevän avarakatseisella tavalla.
+</p>
+<table>
+    <tr>
+    <td> Nimi: </td>
+    <td> <input name="nimi4" size="30" maxlength="100"/></td>
+    </tr><tr>
+    <td> Paikkakunta: </td>
+    <td> <input name="paikkakunta4" size="30" maxlength="50"/></td>
+    </tr><tr> 
+    <td> Perustelut: </td> 
+    <td> <textarea name="perustelut4" rows="4" cols="45" maxlength="1000"> </textarea> </td> 
     </tr> 
 </table>
- 
- <h4>KATEGORIA 4 </h4>
+<h2><strong>Vuoden 2013 maaseutuyritys</strong></h2>
+<p>
+Yritys on innovatiivisella tavalla parantanut maaseudun elämänlaatua ja palveluita. Yritys on myös lisännyt maaseudun elinvoimaisuutta, yhteisöllisyyttä ja parantanut maaseudun viihtyisyyttä. Yritys voi olla maatila tai muu maaseudulla toimiva yritys.
+</p>
 <table>
     <tr>
     <td> Nimi: </td>
-    <td> <input name="nimi" size= "10" /></td>
-    </tr>
-	<tr>
-	<td> Paikkakunta: </td>
-    <td> <input name="paikkakunta" size= "10" /></td>
-	</tr> 
-    <tr> 
-	<td> Perustelut: </td> 
-	<td> <textarea rows = "4" cols="45"> </textarea> </td> 
+    <td> <input name="nimi5" size="30" maxlength="100"/></td>
+    </tr><tr>
+    <td> Paikkakunta: </td>
+    <td> <input name="paikkakunta5" size="30" maxlength="50"/></td>
+    </tr><tr> 
+    <td> Perustelut: </td> 
+    <td> <textarea name="perustelut5" rows="4" cols="45" maxlength="1000"> </textarea> </td> 
     </tr> 
 </table>
 
+<input type="submit" value="Lähetä ehdotukset" />
+</form>
