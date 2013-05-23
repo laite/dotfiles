dca347cb28ee76d7eb3a89099a17067e63033680
Markus Klinga <laite@gmx.com>
2013-05-23 14:31:51 +0300

    add workspace to backuplist

--- backup_all.sh
+++ backup_all.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+echo "Backuping home"
 /home/laite/bin/backup_script.sh /home/laite/ /data/backups/home /home/laite/bin/backup_exclude.list 
+echo "Backuping Documents"
 /home/laite/bin/backup_script.sh /data/str/Documents/ /data/backups/Documents
+echo "Backuping workspace"
+/home/laite/bin/backup_script.sh /data/str/workspace/ /data/backups/workspace
