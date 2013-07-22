673d394479dd7997fadcecc186c9889a7ed7ab43
Markus Klinga <laite@gmx.com>
2013-07-22 10:46:38 +0300

    check for artist images, propose removing empty directories

--- addtolibrary.sh
+++ addtolibrary.sh
@@ -8,7 +8,7 @@ NC='\e[0m' # no color
 if [ $# -ne 1 ]
 then
 	echo -e "$0: ${red}Bad arguments!${NC}"
-    echo -e "usage: $0 directory"
+	echo -e "usage: $0 directory"
 	exit;
 fi
 
@@ -25,5 +25,33 @@ echo -e "${red}:: ABORT - copying did NOT succeed ::${NC}"
 exit
 }
 
-echo -e "${cyan}:: Removing original $source${NC}"
+# find out if there's an artist image in folder
+# it should be "folder.jpg" or "folder.png"
+topfolder=$(echo "$source" | sed 's#\(/data/str/_Music/[^/]*\).*#\1#')
+
+echo ":: Looking for artist image in $topfolder..."
+if [ -f "$topfolder/folder.png" ];
+then
+	echo -e "${cyan}:: Found artist image! \"$topfolder/folder.png\"${NC}"
+	mv -v "$topfolder/folder.png" "$(echo $topfolder | sed 's/_Music/Music/')"
+elif [ -f "$topfolder/folder.jpg" ];
+then
+	echo -e "${cyan}:: Found artist image! \"$topfolder/folder.jpg\"${NC}"
+	mv -v "$topfolder/folder.jpg" "$(echo $topfolder | sed 's/_Music/Music/')"
+else
+	echo ":: No image found."
+fi
+
+# remove original folder (with user confirmation)
+echo -e "${red}:: Removing original $source${NC}"
 rm -Ivr "$source"
+
+# if original topfolder is empty, ask also to remove it
+echo ":: $topfolder has still following items"
+ls -lA "$topfolder"
+
+if find "$topfolder" -maxdepth 0 -empty | read;
+then
+	echo -e "${red}:: Removing $topfolder ::${NC}"
+	rm -Ivr "$topfolder"
+fi
