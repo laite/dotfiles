4f4fe2c0d7a467300880413139b3387f087d1c39
Markus Klinga <laite@gmx.com>
2014-04-26 10:38:36 +0300

    make aliases global

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git config alias.st 'status -sb'
-git config alias.hi 'log --pretty=format:"%C(yellow)%h%x09%Creset%ad %C(blue)%an%Creset %s" --date=short --decorate --graph'
+git config --global alias.st 'status -sb'
+git config --global alias.hi 'log --pretty=format:"%C(yellow)%h%x09%Creset%ad %C(blue)%an%Creset %s" --date=short --decorate --graph'
