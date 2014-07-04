002a956e8cebe1f6369a92d642558acb3e826178
Markus Klinga <laite@gmx.com>
2014-07-04 21:02:24 +0300

    generalize options a bit

--- rss.sh
+++ rss.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-chan="irc.Quakenet.#lobsterfort"
+CHANNEL="" # irc.server.#channel
+TOKEN="" # fill in your token
 
 while [ true ] ;
 do
 	info=()
 	i=0
-	rsstail -i 15 -l -u http://95.85.23.194/w/tiki-wiki_rss.php?TOKEN=4efda3f3e420ed4a175dcc56d5741a30 -n 0 -a | while read x ;
+	rsstail -i 15 -l -u http://95.85.23.194/w/tiki-wiki_rss.php?TOKEN=$TOKEN -n 0 -a | while read x ;
 	do
 		#Set the field separator to new line
 		IFS=$'\n'
@@ -21,7 +22,7 @@ do
 			info[1]=${info[1]/Link: /}
 			#info[2]=${info[2]/Author: /}
 			info[2]="Someone"
-			echo "$chan *:: LobsterWiki :: ${info[2]} changed page \"${info[0]}\" - ${info[1]}" > /home/laite/.weechat/weechat_fifo_3061 ;
+			echo "$CHANNEL *:: LobsterWiki :: ${info[2]} changed page \"${info[0]}\" - ${info[1]}" > /home/laite/.weechat/weechat_fifo_3061 ;
 		fi
 	done
 done 
