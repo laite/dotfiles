cce440373b6f9ba3b3627dc1ab60a9f5e7521288
Markus Klinga <markus.klinga@sc5.io>
2014-08-28 14:34:24 +0300

    add refs to git hi

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git config --global alias.st 'status -sb'
-git config --global alias.hi 'log --pretty=format:"%C(yellow)%h%x09%Creset%ad %C(blue)%an%Creset %s" --date=short --decorate --graph'
+git config --global alias.hi 'log --pretty=format:"%C(yellow)%h%x09%Creset%ad %C(blue)%an%Creset %s%C(yellow)%d%Creset" --date=short --decorate --graph'
